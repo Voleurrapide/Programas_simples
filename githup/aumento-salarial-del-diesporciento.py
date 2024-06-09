85258ce191e376aa11a8e866b70414af8aa86b00
Voleurrapide <mathiasalvarezcanela@gmail.com>
2024-06-08 22:07:09 -0400

    Add files via upload

---
+++ githup/aumento-salarial-del-diesporciento.py
@@ -0,0 +1,5 @@
+si=1500
+sf=si
+for i in range(6):
+    sf=sf*1.10
+print("El salario inicial es de ",si,"$ y el salario a cabo de 6 años es de ",sf,"$")
---
+++ githup/metodo-de-pago.py
@@ -0,0 +1,7 @@
+I=19
+P=5
+for i in range(I-1):
+    P=P*2
+    print(P)
+T=P
+print("El coste acumulado es de ",T)
