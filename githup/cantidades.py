d52d2cc934a892e55486a76593e9354e127029ee
Voleurrapide <mathiasalvarezcanela@gmail.com>
2024-06-09 16:29:25 -0400

    Add files via upload

--- githup/cantidades.py
+++ githup/cantidades.py
@@ -1,13 +1,13 @@
-cantidad=int(input("La cantidades que tene --> "))
+cantidad=int(input("La cantidades que tiene --> "))
 
 for i in range(cantidad-1):
-    cantidad=int(input("Cual es su cantidad n",i+1))
+    cantidad=int(input("Cual es su cantidad numero",i+1))
     if cantidad>0:
         incero=incero+1
     elif cantidad==0:
         cero=cero+1
     elif cantidad<0:
         supcero=supcero+1
-print("su cantidad es superior a 0 es de ",supcero)
-print("Su cantidad es nula en 0",cero)
-print("Su cantidad es menor a 0",incero)
+print("Sus cantidades superiores a 0 son de ",supcero)
+print("Sus cantidades iguales a 0 son de ",cero)
+print("Sus cantidades menores a 0 son de ",incero)
--- githup/presio-en-descuento.py
+++ githup/presio-en-descuento.py
@@ -1,9 +1,9 @@
-c=int(input("cuantos articolos esta tomando--> "))
-p=float(input("A que presio esta --> "))
+c=int(input("Cuantos artículos esta tomando --> "))
+p=float(input("A que precio esta --> "))
 cpf=c*p
 if cpf>=200:
-    print("Usted tiene un descuento del 15%, lo cual resultaria en un presio final de ",cpf*0.85)
+    print("Usted tiene un descuento del 15%, lo cual resulta en un presio final de ",cpf*0.85)
 elif cpf<200 and cpf>=100:
-    print("Usted tiene un descuento del 12%, lo cual resultaria en un presio final de ",cpf*0,88)
+    print("Usted tiene un descuento del 12%, lo cual resulta en un presio final de ",cpf*0,88)
 elif cpf<100:
-    print("Usted tiene un descuento del 10%, lo cual resultaria en un presio final de ",cpf*0,90)
+    print("Usted tiene un descuento del 10%, lo cual resulta en un presio final de ",cpf*0,90)
