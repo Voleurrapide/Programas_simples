0ef8491c491be9d379da671d98643eb226ae038b
Voleurrapide <mathiasalvarezcanela@gmail.com>
2024-06-16 18:58:55 -0400

    Add files via upload

---
+++ githup/lista-de-cuadrados.py
@@ -0,0 +1,2 @@
+for i in range(10):
+    print("El cuadrado del elemento ",i+1," es ",(i+1)**2)
---
+++ githup/numero-impar.py
@@ -0,0 +1,6 @@
+c=0
+i=-1
+while c<10:
+    c=c+1
+    i=i+2
+    print("El numero impar n",c," es el ",i)
